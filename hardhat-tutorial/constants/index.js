2190c1962b4386203f68dae2dc8226c9ea63590d
CodeIndifferent <84685307+CodeIndifferent@users.noreply.github.com>
2022-07-28 11:06:16 -0400

    Updates

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xd7f056Fc26bF096f32F18ab64201bBBF89b9EaC3";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
